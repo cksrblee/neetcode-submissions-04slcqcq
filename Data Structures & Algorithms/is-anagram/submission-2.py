f2dfdd3282c3c441a8ecd2798c766ab3b30d7c33
Lee Changyu <71711381+cksrblee@users.noreply.github.com>
2026-09-05 14:19:01 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        seen = defaultdict(int)
+
+        #init 
+        for c in s:
+            seen[c] += 1
+        
+        for c in t:
+            seen[c] -= 1
+        
+        for i in seen.values():
+            if i != 0:
+                return False
+        
+        return True
